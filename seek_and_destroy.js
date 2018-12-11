ac084b14182d73eed4fb055fdec7b6511eb5a736
Michał Stróż <michalstroz@gmail.com>
2018-12-11 19:59:47 +0100

    Remove all elements from the initial array that are of the same value as given arguments.

---
+++ seek_and_destroy.js
@@ -0,0 +1,9 @@
+function destroyer(arr, ...args) {
+  return arr.filter((val) => !args.includes(val));
+}
+
+destroyer([1, 2, 3, 1, 2, 3], 2, 3);
+destroyer([3, 5, 1, 2, 2], 2, 3, 5);
+destroyer([2, 3, 2, 3], 2, 3);
+destroyer(["tree", "hamburger", 53], "tree", 53);
+destroyer(["possum", "trollo", 12, "safari", "hotdog", 92, 65, "grandma", "bugati", "trojan", "yacht"], "yacht", "possum", "trollo", "safari", "hotdog", "grandma", "bugati", "trojan");
